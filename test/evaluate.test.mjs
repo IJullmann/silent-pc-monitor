34dc2f63f7c623021054d4a1d01d4360b8f1f16b
IJullmann <jullmann@gmx.com>
2026-08-22 13:47:08 +0200

    Add eBay and refurbished sources

--- test/evaluate.test.mjs
+++ test/evaluate.test.mjs
@@ -4,6 +4,7 @@ import { canonicalId, evaluateAd, parsePrice } from '../lib/evaluate.mjs';
 
 test('parses German offer prices', () => assert.equal(parsePrice('Preis 1.250 € VB'), 1250));
 test('uses stable Kleinanzeigen id', () => assert.equal(canonicalId('https://www.kleinanzeigen.de/s-anzeige/a/123-456-789?x=1'), '123-456-789'));
+test('uses stable eBay id', () => assert.equal(canonicalId('https://www.ebay.de/itm/Silent-PC/188047936595?x=1'), '188047936595'));
 test('accepts a complete, active Silent-PC', () => {
   const item = evaluateAd({ active: true, title: 'Silentmaxx 0dB PC', description: 'Ryzen 7 5700G, 32 GB DDR4 RAM, 1 TB NVMe, 3 Monitore, komplett passiv', price: 525, url: 'x' });
   assert.equal(item.matches, true);
